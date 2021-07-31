d99697e1d81f543643bd655149c39734183b45a8
MahoMuri <paolo.santos_12@yahoo.com.ph>
2021-07-31 22:28:11 +0800

    finished 8 ball animations and transitions

--- app/src/main/java/com/example/magic8ball/MainActivity.kt
+++ app/src/main/java/com/example/magic8ball/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.magic8ball
 
+import android.graphics.drawable.TransitionDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var button : Button
@@ -19,8 +22,36 @@ class MainActivity : AppCompatActivity() {
         button = findViewById(R.id.btn)
         image = findViewById(R.id.magic8ball)
 
-        button.setOnClickListener(View.OnClickListener {
-            image.startAnimation(AnimationUtils.loadAnimation(this, R.anim.shake))
-        })
+        button.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(this, R.anim.shake)
+            image.startAnimation(animation)
+
+            animation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation?) {
+                    image.setImageResource(R.drawable.magic_8_ball)
+                }
+
+                override fun onAnimationEnd(animation: Animation?) {
+                    val td = TransitionDrawable(
+                        arrayOf(
+                            ResourcesCompat.getDrawable(resources, R.drawable.magic_8_ball, theme),
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.magic_8_ball_response,
+                                theme
+                            )
+                        )
+                    )
+                    image.setImageDrawable(td)
+                    td.startTransition(500)
+
+                }
+
+                override fun onAnimationRepeat(animation: Animation?) {
+                }
+
+            })
+
+        }
     }
 }
